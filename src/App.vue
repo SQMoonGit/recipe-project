a99d7847fa94b243774f70f1a1ae3254e36ae38a
SQMoonGit <hykmoon@gmail.com>
2021-04-15 23:50:41 -0500

    More setup

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <recipe-view />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
+import RecipeView from "./views/RecipeView.vue";
 
 @Component({
   components: {
-    HelloWorld,
+    RecipeView,
   },
 })
 export default class App extends Vue {}
---
+++ src/views/RecipeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>I've found a new recipeh</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({})
+export default class RecipeView extends Vue {}
+</script>
+
+<style lang="scss">
+@media screen and (min-width: 1024px) {
+}
+@media screen and (min-width: 768px) and (max-width: 1023px) {
+}
+@media screen and (max-width: 767px) {
+}
+</style>
