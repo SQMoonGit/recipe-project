86d2513434a793923d5f62b445b2b46a1ced6cd8
SQMoonGit <hykmoon@gmail.com>
2021-05-03 16:59:49 -0500

    Added responsive styling

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="home-view">
-    <div class="title">The Recipeh Project</div>
-    <button class="add-button">Add Recipe</button>
+    <div class="title">
+      <button class="add-button">Remove Recipe</button>
+      <span>The Recipeh Project</span>
+      <button class="add-button">Add Recipe</button>
+    </div>
     <div class="recipe-list">
+      <img class="recipe-pic" />
       <router-link
         class="recipe-items"
         v-for="(v, i) in recipes"
@@ -32,42 +36,71 @@ export default class HomeView extends Vue {
 </script>
 
 <style lang="scss">
+.title {
+  display: flex;
+  justify-content: space-evenly;
+}
+
+.recipe-list {
+  display: flex;
+  flex-flow: column wrap;
+
+  .recipe-items {
+    color: black;
+    text-decoration: none;
+
+    &:hover {
+      background-color: silver;
+    }
+
+    &:active {
+      color: white;
+      background-color: royalblue;
+      text-decoration: underline;
+    }
+  }
+}
+.add-button {
+  cursor: pointer;
+  font-size: 18px;
+  border-radius: 4rem;
+  border: 1px solid white;
+  background-color: royalblue;
+  color: white;
+
+  &:hover {
+    background-color: #91a8ee;
+  }
+}
+
 @media screen and (min-width: 1024px) {
+  .title {
+    font-size: 3rem;
+  }
+
+  .recipe-list .recipe-items {
+    font-size: 2.5rem;
+  }
 }
 @media screen and (min-width: 768px) and (max-width: 1023px) {
+  .title {
+    font-size: 2.5rem;
+  }
+
+  .recipe-list .recipe-items {
+    font-size: 2rem;
+  }
 }
 @media screen and (max-width: 767px) {
   .title {
     font-size: 2rem;
   }
 
-  .add-button {
-    position: fixed;
-    right: 1rem;
+  .recipe-list .recipe-items {
     font-size: 1.5rem;
-    border-radius: 4rem;
-    border: 1px solid white;
-    background-color: royalblue;
-    color: white;
-  }
 
-  .recipe-list {
-    margin: 0.5rem 0;
-    .recipe-items {
-      display: block;
-      font-size: 1.5rem;
-      color: black;
-      text-decoration: none;
-
-      &:hover {
-        background-color: silver;
-      }
-
-      &:active {
-        color: white;
-        background-color: royalblue;
-        text-decoration: underline;
-      }
+    .recipe-pic {
+      display: none;
     }
   }
 }
